6e51cd3f55599b6b427806446f5d794896b5a301
sonyasergeeva <sonya451f@gmail.com>
2025-04-13 19:46:24 +0700

    Add search.html

--- parser/forms.py
+++ parser/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(
-        label='Введите запрос',
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        label='',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Например: велосипед',
+            'class': 'w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500'
+        })
     )
